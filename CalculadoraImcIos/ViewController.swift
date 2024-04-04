8ca291495099de2ae542e2800d3bff20456c372b
bperezll <borja.perez.llano@gmail.com>
2024-04-04 12:25:49 +0200

    Functional BMI calculator

--- CalculadoraImcIos/ViewController.swift
+++ CalculadoraImcIos/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Variable outlets to use in the code
+    @IBOutlet var heightResult: UILabel!
+    @IBOutlet var weightResult: UILabel!
+    @IBOutlet var calculateResult: UILabel!
+    @IBOutlet var stepperOutlet: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Initialization of the value of the stepper
+        stepperOutlet.value = 80
     }
-
-
+    
+    // Display on the height result text the value of the slider without decimals
+    @IBAction func sliderAction(_ sender: UISlider) {
+        heightResult.text = String(format: "%.0f", sender.value)
+    }
+    
+    // Display on the weight result text the value of the stepper without decimals
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        weightResult.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func calculateBtn(_ sender: UIButton) {
+        
+        // Constant variables to use String as Double
+        let heightResultDouble = Double(heightResult.text!)!
+        let weightResultDouble = Double(weightResult.text!)!
+        
+        // Constant variable to calculate te BMI
+        let calculateResultDouble = weightResultDouble / ((heightResultDouble / 100) * (heightResultDouble / 100))
+        
+        // Show the result of the BMI calculation with only one decimal
+        calculateResult.text = (String(format:"%.01f", calculateResultDouble))
+        
+        // Print on console the result with all the decimals
+        print("IMC: \(calculateResultDouble)")
+    }
 }
 
